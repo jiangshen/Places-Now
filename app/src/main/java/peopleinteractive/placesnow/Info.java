969a697d1d903167a8b3c4f3be362c535b4c3ce1
Dieci, Estella <estella@dieci-mathm1507.local>
2016-07-21 16:57:41 -0400

    working on popup

--- app/src/main/java/peopleinteractive/placesnow/Info.java
+++ app/src/main/java/peopleinteractive/placesnow/Info.java
@@ -1,17 +1,26 @@
 package peopleinteractive.placesnow;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import java.util.ArrayList;
 
 public class Info extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,4 +52,15 @@ public class Info extends AppCompatActivity {
 //        myList.setAdapter(commentAdapter);
     }
 
+    public void popup() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View popupView = inflater.inflate(R.layout.popup,
+                null, false);
+
+        final EditText comments = (EditText) popupView
+                .findViewById(R.id.textBox);
+
+        // the alert dialog
+        new AlertDialog.Builder(Info.this).setView(popupView).show();
+    }
 }
