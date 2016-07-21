55ed230194ae95d4b3f223e8af8c6da045cc7199
Dieci, Estella <estella@dieci-mathm1507.local>
2016-07-21 19:46:38 -0400

    Pop-up to add comments works, could not quite figure out how to style it well, and also it adds the comment(s) to the comments array

--- app/src/main/java/peopleinteractive/placesnow/Info.java
+++ app/src/main/java/peopleinteractive/placesnow/Info.java
@@ -1,18 +1,30 @@
 package peopleinteractive.placesnow;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+
 import java.util.List;
 
 import java.util.ArrayList;
 
 public class Info extends AppCompatActivity {
+
+    List<Comment> commentArray = new ArrayList<Comment>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +37,10 @@ public class Info extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Log.d("CLICKED FAB", "clicked");
+                displayPopup();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -45,5 +59,37 @@ public class Info extends AppCompatActivity {
 //        ListView myList=
 //                (ListView) findViewById(R.id.commentListView);
 //        myList.setAdapter(commentAdapter);
+    }
+
+    private void displayPopup() {
+        Log.d("popup", "trying to display");
+
+        final EditText editText = new EditText(Info.this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Info.this, R.style.DialogStyle);
+        LayoutInflater inflater = Info.this.getLayoutInflater();
+
+        builder.setTitle("Add A Comment")
+                .setView(editText)
+        .setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        })
+        .setNegativeButton("Submit", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                String comment = editText.getText().toString();
+                commentArray.add(new Comment(comment));
+                Log.d("COMMENT", comment);
+                Log.d("all comments", commentArray.toString());
+                dialog.cancel();
+            }
+        })
+        .show();
+    }
+
+    private void exitPopup(View view) {
+        startActivity(new Intent(this, Info.class));
     }
 }
